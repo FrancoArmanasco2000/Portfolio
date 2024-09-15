fd9b6f570c3c05600374d8fe7471168d9cbdcf04
Franco Armanasco <128731027+FrancoArmanasco2000@users.noreply.github.com>
2024-09-15 13:19:17 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,6 @@ export function About() {
         <div className="about-container" id='sobremi'>
             <h1>Sobre mi</h1>
             <p>
-                
 Soy un desarrollador de software con experiencia tanto en backend como en frontend. Mi especialidad en backend se basa en el manejo de Java, utilizando tecnologías como Spring Boot y Hibernate para crear soluciones eficientes y escalables. En el ámbito frontend, trabajo con React y Angular, lo que me permite diseñar interfaces dinámicas y atractivas. Mi enfoque es ofrecer soluciones integrales, participando en todas las etapas del desarrollo para asegurar una ejecución coherente y de calidad en cada proyecto.
             </p>
         </div>
