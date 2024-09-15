8b5cdaf4516d98f0f978cbf73f50dc450581ee67
Franco Armanasco <128731027+FrancoArmanasco2000@users.noreply.github.com>
2024-09-15 13:18:02 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ export function About() {
         <div className="about-container" id='sobremi'>
             <h1>Sobre mi</h1>
             <p>
-                Soy un desarrollador con experiencia especializada en la creación de software y aplicaciones web. Mi enfoque se centra en el desarrollo de software y backend utilizando Java, con un manejo sólido de herramientas como Spring Boot, Spring Security y Hibernate para garantizar la eficiencia y la escalabilidad del código. Además, poseo una amplia experiencia en el manejo de bases de datos SQL (PostgreSQL), lo que me permite diseñar sistemas robustos y seguros. También tengo habilidades en la creación de interfaces gráficas de usuario utilizando Swing y JavaFX. En el mundo móvil, desarrollo aplicaciones Android utilizando Kotlin. Participo en cada etapa de la programación, desde la creación de diagramas hasta la finalización del proyecto programado, asegurando una ejecución integral y coherente de principio a fin.
-                En el ámbito del frontend, cuento con habilidades avanzadas en el uso de React, así como en la implementación de tecnologías web como JavaScript, HTML y CSS. Esta combinación de habilidades me permite crear interfaces de usuario interactivas y atractivas, que mejoran la experiencia del usuario final.
+                
+Soy un desarrollador de software con experiencia tanto en backend como en frontend. Mi especialidad en backend se basa en el manejo de Java, utilizando tecnologías como Spring Boot y Hibernate para crear soluciones eficientes y escalables. En el ámbito frontend, trabajo con React y Angular, lo que me permite diseñar interfaces dinámicas y atractivas. Mi enfoque es ofrecer soluciones integrales, participando en todas las etapas del desarrollo para asegurar una ejecución coherente y de calidad en cada proyecto.
             </p>
         </div>
     )
